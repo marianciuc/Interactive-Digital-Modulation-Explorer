02460ac3eb9a62e17167fa9dea97dce44f6303fa
Vladimir Marianciuc <39910684+marianciuc@users.noreply.github.com>
2024-06-23 16:26:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -10,4 +10,5 @@ app.layout = create_layout()
 register_callbacks(app)
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    port = int(os.environ.get('PORT', 8050))
+    app.run_server(debug=False,  host='0.0.0.0', port=port)
